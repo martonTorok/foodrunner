059f65979d6533427940efcb5e75610cf2b9f07b
tormartonz <tormartonz@gmail.com>
2019-02-05 13:10:14 +0100

    added item routes to app constructor

--- src/app.ts
+++ src/app.ts
@@ -3,17 +3,20 @@ import * as bodyParser from 'body-parser';
 import * as cors from 'cors';
 import { Sequelize } from 'sequelize-typescript';
 import { UserRoutes } from './routes/user.routes';
+import { ItemRoutes } from './routes/item.routes';
 
 
 class App {
     public app: express.Application;
     private userRoutes: UserRoutes = new UserRoutes();
-    
+    private itemRoutes: ItemRoutes = new ItemRoutes();
+
     constructor() {
         this.app = express();
         this.config();
         this.dbSetup();
         this.userRoutes.routes(this.app);
+        this.itemRoutes.routes(this.app);
     }
 
     public listen(): void {
