815f89cb3cc6182f8eb7ce42b7f90afb49096577
tormartonz <tormartonz@gmail.com>
2019-02-05 12:05:02 +0100

    added main app

---
+++ src/app.ts
@@ -0,0 +1,36 @@
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import * as cors from 'cors';
+import { Sequelize } from 'sequelize-typescript';
+
+
+class App {
+    public app: express.Application;
+    
+    constructor() {
+        this.app = express();
+    }
+
+    public listen(): void {
+        this.app.listen(process.env.PORT, () => {
+            console.log(`Server listening on port: ${process.env.PORT}`);
+        })
+    }
+
+    private config(): void {
+        this.app.use(bodyParser.json());
+        this.app.use(cors());
+    }
+
+    private async dbSetup() {
+        const sequelize = new Sequelize({
+            database: process.env.MYSQL_DATABASE,
+            dialect: process.env.DB_DIALECT,
+            username: process.env.MYSQL_USERNAME,
+            password: process.env.MYSQL_PASSWORD,
+            modelPaths: [__dirname + '/models']
+        });
+    }
+}
+
+export default App;
