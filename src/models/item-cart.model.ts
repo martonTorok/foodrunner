0af23fef75cd291784d228176b64895fe97838f4
tormartonz <tormartonz@gmail.com>
2019-02-05 12:19:52 +0100

    added sequelize cart item model

---
+++ src/models/item-cart.model.ts
@@ -0,0 +1,28 @@
+import {Table, Column, Model, DataType, BelongsTo, ForeignKey} from 'sequelize-typescript';
+
+import Cart from './cart.model';
+import Item from './item.model';
+
+@Table({tableName: 'shoppingCartItems'})
+export default class ItemCart extends Model<ItemCart> {
+
+    @Column(DataType.FLOAT)
+    totalPrice: number;
+
+    @Column(DataType.FLOAT)
+    totalQuantity: number
+
+    @ForeignKey(() => Cart)
+    @Column
+    cartId: number;
+    
+    @BelongsTo(() => Cart)
+    cart: Cart;
+
+    @ForeignKey(() => Item)
+    @Column
+    itemId: number;
+
+    @BelongsTo(() => Item)
+    item: Item;
+}
