cb7be14857149d2ec580b52aa81e8f05f1ce994a
tormartonz <tormartonz@gmail.com>
2019-02-05 17:10:04 +0100

    changed references to service

--- src/controllers/item.controller.ts
+++ src/controllers/item.controller.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from 'express';
 
-import Item from '../models/item.model';
+import * as itemService from '../services/item.service';
 
 export class ItemController {
 
     public async getAllItems(request: Request, response: Response) {
         try {
-            const items = await Item.find();
+            const items = await itemService.getAllItems();
             response.status(200).send(items);
         } catch (e) {
             response.status(404);
@@ -16,8 +16,8 @@ export class ItemController {
     public async getItemsByCategory(request: Request, response: Response) {
         const category = request.query.category;
         try {
-            const products = await Item.findAll({where: { category }});
-            response.status(200).send(products);
+            const items = await itemService.getItemsByCategory(category);
+            response.status(200).send(items);
         } catch (e) {
             response.status(400).send(e);
         }
@@ -26,8 +26,8 @@ export class ItemController {
     public async getItemById(request: Request, response: Response) {
         const id = request.params.id;
         try {
-            const product = await Item.findOne({where:{ id }});
-            response.status(200).send(product);
+            const item = await itemService.getItemById(id);
+            response.status(200).send(item);
         } catch (e) {
             response.status(400).send(e);
         }
